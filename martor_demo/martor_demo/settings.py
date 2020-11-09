2c49ac5ba5c534dbe61cf0cac39a0b2bc1e73c16
wivill <willy.villalobos@ucr.ac.cr>
2020-11-08 19:37:00 -0600

    fixing flake8 style issues

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-# import tempfile
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,7 +140,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-# STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
-# MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
