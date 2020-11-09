eeae683133dd71662ad8c4fcea4900e97bc8a526
wivill <willy.villalobos@ucr.ac.cr>
2020-11-08 19:23:24 -0600

    fixing flake8 style issues

--- martor_demo/app/testPosts.py
+++ martor_demo/app/testPosts.py
@@ -9,5 +9,4 @@ class PostTestCase(TestCase):
         post = Post(title="My Title", description="Blurb", wiki="Post Body")
         self.assertEqual(post.title, "My Title")
         self.assertEqual(post.description, "Blurb")
-        self.assertEqual(post.wiki, "Post Body")
-        
+        self.assertEqual(post.wiki, "Post Body") 
--- martor_demo/app/views.py
+++ martor_demo/app/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import (render, redirect)
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
